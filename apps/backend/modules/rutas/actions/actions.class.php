278952be3b72c92de8743e0ae9362030f12a2972
Juan Fernando Obando Saldarriaga <juanchopx2@gmail.com>
2011-04-18 14:48:45 -0500

    adicionado al backend el módulo rutas

---
+++ apps/backend/modules/rutas/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * rutas actions.
+ *
+ * @package    drubu
+ * @subpackage rutas
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class rutasActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
