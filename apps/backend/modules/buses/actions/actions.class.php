19b217ce277edf708d6f472e771eed85533231c8
Juan Fernando Obando Saldarriaga <juanchopx2@gmail.com>
2011-04-24 21:45:06 -0500

    adicionado al backend el módulo buses

---
+++ apps/backend/modules/buses/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/busesGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/busesGeneratorHelper.class.php';
+
+/**
+ * buses actions.
+ *
+ * @package    drubu
+ * @subpackage buses
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class busesActions extends autoBusesActions
+{
+}
---
+++ apps/backend/modules/buses/lib/busesGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * buses module helper.
+ *
+ * @package    drubu
+ * @subpackage buses
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class busesGeneratorHelper extends BaseBusesGeneratorHelper
+{
+}
