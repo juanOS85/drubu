bda3a15af3d84adbe66bd2023347e94c11d016d3
Juan Fernando Obando Saldarriaga <juanchopx2@gmail.com>
2011-05-22 17:34:31 -0500

    archivos generados automaticamente por la tarea de symfony, doctrine:build --all

---
+++ lib/form/doctrine/busForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * bus form.
+ *
+ * @package    drubu
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class busForm extends BasebusForm
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/rutaForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ruta form.
+ *
+ * @package    drubu
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class rutaForm extends BaserutaForm
+{
+  public function configure()
+  {
+  }
+}
